67701b13047b7ef35de52785aa0cca823aa7b9e4
Tarun Verghis <tarun.verghis@gmail.com>
2025-05-06 22:31:08 -0700

    Display AccessPoint information

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+mod access_point;
 mod device;
 mod network_manager;
 mod wireless;
 
 use std::collections::HashMap;
 
+use access_point::AccessPointProxy;
 use device::DeviceProxy;
 use futures_util::stream::StreamExt;
 use wireless::WirelessProxy;
@@ -16,7 +18,7 @@ async fn main() -> Result<()> {
     let x = proxy.get_devices().await?;
 
     for dev in x.iter() {
-        let device = device_props(&connection, &dev).await?;
+        let device = device_props(&connection, dev).await?;
 
         if let Device::Wireless(wifi_device) = device {
             wifi_device.scan().await?;
@@ -39,11 +41,13 @@ struct WifiDevice<'a> {
 
 impl<'a> WifiDevice<'a> {
     async fn scan(&self) -> Result<()> {
+        // Request a scan; this will send a PropertyChanged signal for the "LastScan" property once completed.
         self.proxy.request_scan(HashMap::new()).await?;
         let mut scan_changed_notif = self.proxy.receive_last_scan_changed().await;
 
         let mut last_scan_time: Option<i64> = None;
 
+        // Wait for last_scan_time to change before proceeding
         while let Some(signal) = scan_changed_notif.next().await {
             let signal = signal.get().await?;
             if let Some(prev) = last_scan_time {
@@ -54,8 +58,17 @@ impl<'a> WifiDevice<'a> {
             last_scan_time = Some(signal);
         }
 
+        // At this point, we can query the updated list of access points that were retrieved from this scan.
         let access_pts = self.proxy.get_access_points().await?;
-        dbg!(access_pts);
+        for ap in access_pts.iter() {
+            let ap_proxy = AccessPointProxy::builder(self.proxy.inner().connection())
+                .path(ap)?
+                .build()
+                .await?;
+            let ssid = unsafe { String::from_utf8_unchecked(ap_proxy.ssid().await?) };
+            let freq = ap_proxy.frequency().await? as f32 / 1000.0;
+            println!("{ssid} ({freq:.1}GHz)");
+        }
 
         Ok(())
     }
